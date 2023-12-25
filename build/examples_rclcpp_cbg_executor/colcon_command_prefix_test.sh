4205a2096799db893c0de9675033bce0a6628f40
AndreyKarmanov <Andrey@Karmanov.ca>
2023-12-24 23:50:03 -0500

    first commit

---
+++ build/examples_rclcpp_cbg_executor/colcon_command_prefix_test.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/andrey/Documents/workspace/install/examples_rclcpp_cbg_executor/share/examples_rclcpp_cbg_executor/package.sh"
---
+++ build/launch_testing_examples/share/launch_testing_examples/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/andrey/Documents/workspace/build/launch_testing_examples"
